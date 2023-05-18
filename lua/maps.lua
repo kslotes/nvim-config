37fa80a0bd88b65490de5c538a94a46ba905cce0
agustinm <agustinm@unitech.com.ar>
2023-05-18 18:01:59 -0300

    Setting up structure, global config and maps

---
+++ lua/maps.lua
@@ -0,0 +1,6 @@
+local g = vim.g
+local o = vim.o 
+
+-- Map Leader
+g.mapleader = " "
+g.maplocalleader = " "
---
+++ lua/settings.lua
@@ -0,0 +1,18 @@
+local g = vim.g
+local o = vim.o
+
+vim.scriptenconding = "utf-8"
+
+
+-- Editor Config
+o.number = true
+o.relativenumber = true
+o.clipboard = 'unnamedplus'
+o.syntax = 'on'
+o.autoindent = true
+o.cursorline = true
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4 
+
+
