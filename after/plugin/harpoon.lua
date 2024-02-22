2eb346bd6360d21384574fa2d906b32ed3c4ef59
agustinm <agustinm@unitech.com.ar>
2024-02-22 12:37:28 -0300

    remap harpoon to switch between dvorak and qwerty

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,6 +6,23 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-;>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(5) end)
-vim.keymap.set("n", "<C-m>", function() ui.nav_file(6) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-m>", function() ui.nav_file(5) end)
+
+function Remap(number)
+    if number == 1 then
+        vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<C-m>", function() ui.nav_file(5) end)
+    end
+
+    if number == 2 then
+        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<C-b>", function() ui.nav_file(5) end)
+    end
+end
